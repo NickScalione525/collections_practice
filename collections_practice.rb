39c27af6f8f6541f6a11830bf74432e4549b3774
nick <nicholas_scalione@yahoo.com>
2021-01-17 23:20:52 -0500

    commit tree 8ad82a9fab015daec07531ef1c9c4a1115130baa

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,62 @@
+require 'pry'
 
+def sort_array_asc(array)
+array.sort
+end
+
+def sort_array_desc(reverse_array)
+    reverse_array.sort do |a, b|
+        b <=> a
+    end
+end
+
+def sort_array_char_count(array)
+    array.sort do |a, b|
+        if a.size == b.size
+            0
+        elsif a.size < b.size
+        -1
+        elsif
+            a.size > b.size
+            1
+        end
+    end
+end
+
+def swap_elements(array)
+    array.sort do |a, b|
+        if a.size == b.size
+            0
+        elsif a.size < b.size
+        -1
+        elsif
+            a.size > b.size
+            1
+        end
+    end
+end
+
+def reverse_array(array)
+    array.reverse!
+end
+
+def kesha_maker(array)
+    array.each_with_index { |k| k[2] = "$" }
+end
+
+def find_a(array)
+  array.select { |i| i.start_with? ("a") } 
+end
+
+def sum_array(array)
+    array.inject(0) { |sum,x| sum + x }
+end
+
+
+def add_s(array)
+    notouch = array[1]
+    copy = array.each_with_index.collect { |element, i| element + "s" }
+    copy[1] = notouch
+    copy
+    
+end
